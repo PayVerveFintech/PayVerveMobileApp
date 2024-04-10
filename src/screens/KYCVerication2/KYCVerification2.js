158f1e520ff84dde2572b2a2b524ac370c9b44c9
Blessing Onwunali <chikaonwunali2019@gmail.com>
2024-04-10 20:39:25 +0100

    Dayo and Olamide Stashing

--- src/screens/KYCVerication2/KYCVerification2.js
+++ src/screens/KYCVerication2/KYCVerification2.js
@@ -1,5 +1,5 @@
 import React from 'react' 
-import { View, Text, TouchableOpacity } from "react-native"
+import { View, Text, Image, TouchableOpacity } from "react-native"
 import { styles } from "../../styles/styles";
 
 const KYCVerification2 = ({navigation}) => {
